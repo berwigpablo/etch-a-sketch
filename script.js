756afa471a221d00492492551f1f194023598560
pablo-computer <berwigpablo@gmail.com>
2022-06-30 12:57:19 -0300

    Added button for prompting different sizes

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 let container = document.getElementById('container');
-let grid = prompt('Choose grid size:');
-grid *= grid;
-let gridSize = Math.sqrt(640000 / grid);
+let button = document.getElementById('button');
+
+document.addEventListener('DOMContentLoaded', createGrid());
+button.addEventListener('click', createGrid);
 
 let mouseDown = false;
 document.body.onmousedown = () => mouseDown = true; //variable for checking if mouse down at same time as mouse over
 document.body.onmouseup = () => mouseDown = false;
+
+function initialize(){
+    container.innerHTML = '';
+    let grid = prompt('Choose grid size:');
+    grid *= grid;
+    console.log(grid);
+    return grid;
+}
 
 function addGrid(e){
     if(e.type === 'mouseover' && !mouseDown) return; //testing if mousedown + mouseover
@@ -13,20 +22,22 @@ function addGrid(e){
     this.classList.add('grid');
 }
 
-function removeGrid(){
-    this.classList.remove('grid');
-}
-
-for (i = 0; i < grid; i++){
-    let gridBlock = document.createElement('div');
+function createGrid(){
+  let grid = initialize();
+    for (i = 0; i < grid; i++){
+        let gridBlock = document.createElement('div');
     
-    container.appendChild(gridBlock);
+        container.appendChild(gridBlock);
 
-    gridBlock.style.height = `${gridSize}px`;
-    gridBlock.style.width = `${gridSize}px`;
+        let gridSize = Math.sqrt(250000 / grid);
 
-    gridBlock.addEventListener('mousedown', addGrid);
-    gridBlock.addEventListener('mouseover', addGrid);
+        gridBlock.style.height = `${gridSize}px`;
+        gridBlock.style.width = `${gridSize}px`;
+
+        gridBlock.addEventListener('mousedown', addGrid);
+        gridBlock.addEventListener('mouseover', addGrid);
+    }
+  console.log(grid);
 }
 
 
