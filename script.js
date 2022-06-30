c7425bcaa90d9cb578dfbd7bc93fa256a876a4b4
pablo-computer <berwigpablo@gmail.com>
2022-06-30 13:32:58 -0300

    Added default and rainbow modes

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 let container = document.getElementById('container');
 let button = document.getElementById('button');
+let defaux = document.getElementById('default');
+let rainbow = document.getElementById('rainbow');
+let normalMode = true;
+let rainbowMode = false;
 
-document.addEventListener('DOMContentLoaded', createGrid());
 button.addEventListener('click', createGrid);
+defaux.addEventListener('click', setNormal);
+rainbow.addEventListener('click', setRainbow);
 
 let mouseDown = false;
 document.body.onmousedown = () => mouseDown = true; //variable for checking if mouse down at same time as mouse over
 document.body.onmouseup = () => mouseDown = false;
 
+function setNormal(){
+    normalMode = true;
+    rainbowMode = false;
+
+
+    createGrid();
+}
+
+function setRainbow(){
+    normalMode = false;
+    rainbowMode = true;
+
+
+    createGrid();
+}
+
 function initialize(){
     container.innerHTML = '';
+
     let grid = prompt('Choose grid size:');
     grid *= grid;
-    console.log(grid);
+
     return grid;
 }
 
@@ -20,6 +42,18 @@ function addGrid(e){
     if(e.type === 'mouseover' && !mouseDown) return; //testing if mousedown + mouseover
 
     this.classList.add('grid');
+}
+
+function addRainbow(e){
+    if(e.type === 'mouseover' && !mouseDown) return;
+
+    let colorR = Math.floor(Math.random() * 256);
+    let colorG = Math.floor(Math.random() * 256);
+    let colorB = Math.floor(Math.random() * 256);
+
+    console.log(this);
+
+    this.style.backgroundColor = `rgb(${colorR}, ${colorG}, ${colorB})`;
 }
 
 function createGrid(){
@@ -34,11 +68,17 @@ function createGrid(){
         gridBlock.style.height = `${gridSize}px`;
         gridBlock.style.width = `${gridSize}px`;
 
-        gridBlock.addEventListener('mousedown', addGrid);
-        gridBlock.addEventListener('mouseover', addGrid);
+        if (normalMode === true){
+            gridBlock.addEventListener('mousedown', addGrid);
+            gridBlock.addEventListener('mouseover', addGrid);
+        } else {
+            gridBlock.addEventListener('mousedown', addRainbow);
+            gridBlock.addEventListener('mouseover', addRainbow);
+        }
     }
-  console.log(grid);
 }
+
+
 
 
 
