bc5b021ee596d114a807355670a171368f1957f5
Pablo <berwigpablo@gmail.com>
2024-06-04 19:20:20 -0300

    Add variable grid size, up to 100

--- script.js
+++ script.js
@@ -1,37 +1,49 @@
 const container = document.querySelector('.container');
 const button = document.querySelector('button');
 
-button.addEventListener('click', createGrid);
-container.childNodes.forEach(div => div.addEventListener('mouseover', changeColor));
+button.addEventListener('click', chooseSize);
 
+function chooseSize(){
+    if (container.childNodes.length > 0){
+        clearGrid();
+    }
+    const userPrompt = Number(prompt('Choose grid size:'));
+ 
+    createGrid(userPrompt);
+}
 
-function createGrid(){
-    let userPrompt = Number(prompt('Choose grid size:'))
+function clearGrid(){
+    container.childNodes.remove();
+    return
+}
+
+function createGrid(userPrompt){
 
     let grid = userPrompt * userPrompt
 
     for(i = 0; i < grid; i++){
         let div = document.createElement('div');
         container.appendChild(div);
-
+        div.addEventListener('mouseover', changeColor)
     }
 
-    container.childNodes.forEach(gridSize);
+    gridSize(userPrompt);
 }
 
-function gridSize(div){
-    const height = container.clientHeight / userPrompt;
-    const width = container.clientWidth / userPrompt;
+function gridSize(userPrompt){
 
-    div.style.height = `${height}`;
-    div.style.width = `${width}`;
+    container.childNodes.forEach(div => {
+
+        const height = 100 / userPrompt;
+        const width = 100 / userPrompt;
+
+        div.style.height = `${height}%`;
+        div.style.width = `${width}%`;
+
+    });
 }
 
 function changeColor(event){
     const currentDiv = event.target;
     currentDiv.style.backgroundColor = 'black';
 }
-
-
-console.log(container.clientHeight);
-console.log(container.clientWidth);
